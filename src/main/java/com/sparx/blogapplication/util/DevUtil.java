832588c75f446394d34812a05cfc4a0f09585deb
prince <2030709@sliet.ac.in>
2024-06-20 16:05:53 +0530

    Revert "demoTest" -m "i am reverting this commit just for the testing purpose"
    This reverts commit 5316ce4e1b9ffd8bbd9af016413934b19de13742.

--- src/main/java/com/sparx/blogapplication/util/DevUtil.java
+++ src/main/java/com/sparx/blogapplication/util/DevUtil.java
@@ -1,7 +1,5 @@
 package com.sparx.blogapplication.util;
 
 public class DevUtil {
-	public static String   demoTest(String message) {
-		return "this method is being added from dev branch ";
-	}
+
 }
