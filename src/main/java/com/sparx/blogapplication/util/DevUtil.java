5316ce4e1b9ffd8bbd9af016413934b19de13742
prince <2030709@sliet.ac.in>
2024-06-20 12:12:44 +0530

    demoTest

--- src/main/java/com/sparx/blogapplication/util/DevUtil.java
+++ src/main/java/com/sparx/blogapplication/util/DevUtil.java
@@ -1,5 +1,7 @@
 package com.sparx.blogapplication.util;
 
 public class DevUtil {
-
+	public static String   demoTest(String message) {
+		return "this method is being added from dev branch ";
+	}
 }
